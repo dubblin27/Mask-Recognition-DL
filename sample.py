9a46bf7e86300dffc36826c7cf7800dcbe1490d4
Sabrish Kumar P <58567129+dubblin27@users.noreply.github.com>
2020-05-13 18:32:05 +0530

    initial main

--- sample.py
+++ sample.py
@@ -1,30 +1,10 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import numpy
 import tensorflow as tf 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
-import pickle
+# classifierLoad = tf.keras.models.load_model('model/modeltest.h5')
 
-X = pickle.load(open("X.pickle","rb"))
-y = pickle.load(open("y.pickle","rb"))
 
-X = X/255.0 
-
-model = Sequential()
-
-#layer 1
-model.add(Conv2D(64,(3,3),input_shape= X.shape[1:]))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-# layer 2 
-model.add(Conv2D(64,(3,3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-# model.add(layers.Dense(1,activation = 'sigmoid'))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(loss="binary_crossentropy",optimizer="adam",metrics=['accuracy'])
-model.fit(X,y,batch_size=40,validation_split=0.25)
+model = tf.keras.models.load_model('mainmodel.h5')
